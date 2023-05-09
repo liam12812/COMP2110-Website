bfdffb1830cc15281f5e08472749ab057a268e99
Swizbot <bagukhaifsaeed@gmail.com>
2023-05-09 18:31:13 +1000

    fetch function to get reposne

--- Saeed'sWidget/saeedwidget.js
+++ Saeed'sWidget/saeedwidget.js
@@ -17,7 +17,30 @@ class UpcomingPublicHolidays extends LitElement {
    this.country = 'AUS';
    }
 
-   
+   connectedCallback(){
+    this.connectedCallback();
+    this.fetchUpcomingholidays();
+   }
+
+   async fetchUpcomingholidays(){
+    const url = 'https://date.nager.at/api/v2/publicholidays/${new Date().getFullYear()}/${this.country}';
+    const answer = await fetch(url);
+    const data = await response.json();
+    this.upcomingHolidays = data;
+   }
+
+   render(){
+    return html`
+        <h3> Next Public Holidays </h3>
+        <
+
+
+
+
+    `;
+   }
+
+
 
 
     
