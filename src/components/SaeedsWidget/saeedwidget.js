9ed6ea29237c4a78a2749790bcb84767f1e5f79b
Swizbot <bagukhaifsaeed@gmail.com>
2023-05-09 20:54:01 +1000

    shows public holidays but all of them

--- src/components/SaeedsWidget/saeedwidget.js
+++ src/components/SaeedsWidget/saeedwidget.js
@@ -3,59 +3,67 @@ import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core
 class UpcomingPublicHolidays extends LitElement {
 
     static properties = {
-        upcomingholidays:{type: Array},
+        upcomingHolidays:{type: Array},
         country: {type:String}
 
     }
 
-    static MainUrl = "https://date.nager.at/api/v2/publicholidays";
+    static MainUrl = 'https://date.nager.at/api/v2/publicholidays';
 
     static styles = css`
 
     .main {
-        size 20px;
+        size 10px;
     }
    `;
 
    constructor(){
    super();
-   this.upcomingholidays=[];
-   this.country = 'AUS';
+   this.upcomingHolidays= [];
+   this.country = 'AU';
    }
 
    connectedCallback(){
     super.connectedCallback();
-    this.fetchUpcomingholidays();
+    this.fetchUpcomingHolidays();
    }
 
-    fetchUpcomingholidays(){
-    fetch(UpcomingPublicHolidays.MainUrl + "country="+ this.country +"upcomingholidays="+this.upcomingholidays)
+    fetchUpcomingHolidays(){
+    const url = `${UpcomingPublicHolidays.MainUrl}/${new Date().getFullYear()}/${this.country}` ;
+    fetch(url)
     .then(response => response.json())
     .then(data => {
-        this.upcomingHolidays= data
-    })
+        this.upcomingHolidays=data;
+        this.requestUpdate();
+    });
     
    }
+
+    
 
    render(){
     return html`
         <h3> Next Public Holidays </h3>
         <select @change="${this.userCountryChange}">
-            <option value= "AUS"> Australia</option>
-            <option value= "CAN"> Canada</option>
-            <option value= "MEX"> Mexico</option>
-            <option value= "USA"> United States of America</option>
+            <option value= "AU"> Australia</option>
+            <option value= "CA"> Canada</option>
+            <option value= "MX"> Mexico</option>
+            <option value= "US"> United States</option>
+            <option value= "PY"> Paraguay</option>
         </select>
 
         <ul>
-            ${this.upcomingholidays.map((holiday) => html`<li>${holiday.date}: ${holiday.name}</li>`)}
-        </ul>
-    `;
-   }
+          ${this.upcomingHolidays.length === 0
+            ?  html`<li> No Remaining Public Holidays</li>`
+            : this.upcomingHolidays.map(
+                holiday => html`<li>${holiday.date}: ${holiday.name}</li>`)}
+                </ul>
+    `;}
+   
 
    userCountryChange(event){
     this.country= event.target.value;
-    this.fetchUpcomingholidays();
+    this.fetchUpcomingHolidays();
 
    }
     
