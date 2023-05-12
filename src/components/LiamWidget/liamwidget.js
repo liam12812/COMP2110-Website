48bd1d2453471db41a8af0da3479c8d3a3637e38
liam12812 <79492075+liam12812@users.noreply.github.com>
2023-05-12 16:33:17 +1000

    backgrounds fixed

--- src/components/LiamWidget/liamwidget.js
+++ src/components/LiamWidget/liamwidget.js
@@ -10,6 +10,9 @@ class WeatherWidget extends LitElement {
         timezone: {type: String},
         weather_type: {type: String},
         imageUrl: {type: String},
+        weatherCode: {type: String},
+        isday: {type: String},
+        textcolour: {type: String},
 
       }
   
@@ -23,7 +26,7 @@ class WeatherWidget extends LitElement {
 
         #container{
             background-color: #3498DB ;
-            background-image: url(src/images/Clear_Day.png);
+            background-repeat: no-repeat;
             border-radius: 10px;
             position:relative;
             display: block;
@@ -42,6 +45,9 @@ class WeatherWidget extends LitElement {
             bottom: 40px;
             font-weight: bold;
         }
+        #title{
+            font-weight: ;
+        }
     
 
 
@@ -55,6 +61,7 @@ class WeatherWidget extends LitElement {
         this.Longitude = 151.21;
         this.timezone = "Australia%2FSydney";
         this.imageUrl = "src/images/Clear_Day.png"
+
         
       }
 
@@ -66,6 +73,7 @@ class WeatherWidget extends LitElement {
 
     _fetch () {
         const url = `${WeatherWidget.BASE_URL}latitude=${this.Latitude}&longitude=${this.Longitude}&current_weather=true&timezone=${this.timezone}`
+        console.log(url);
         fetch(url)
         .then(response => response.json())
         .then(data => { 
@@ -74,125 +82,263 @@ class WeatherWidget extends LitElement {
         });
         
         
-    }
-
-
+    }    
 
     _weatherText(){
-        let weatherCode = JSON.stringify(this._data.current_weather.weathercode);
-        let isday = JSON.stringify(this._data.current_weather.is_day);
-        if(weatherCode = 0){
+
+        this.weatherCode = "2"//JSON.stringify(this._data.current_weather.weathercode);
+        this.isday = "0"//JSON.stringify(this._data.current_weather.is_day);
+        if(this.weatherCode == 0){
             this.weather_type = "Clear Sky"
-            if(isday = 1){
-                this.imageUrl = "url(src/images/Clear_Day.png)"
+            if(this.isday == "1"){
+                this.imageUrl = "background-image: url(src/images/Clear_Day.png";
+                this.textcolour = "black";
             }
-            else if (isday = 0){
-                this.imageUrl = "url(src/images/Clear_Night.png)"
+            else if (this.isday == "0"){
+                this.imageUrl = "background-image: url(src/images/Clear_Night.png";
+                this.textcolour = "white";
             }
-            css('background-image', this.imageUrl);
         }
-        else if (weatherCode = 1){
+        else if (this.weatherCode == "1"){
             this.weather_type = "Mainly Clear"
-            if(isday = 1){
-                this.imageUrl = "url(src/images/Clear_Day.png)"
+            if(this.isday == "1"){
+                this.imageUrl = "background-image: url(src/images/Clear_Day.png";
+                this.textcolour = "black";
             }
-            else if (isday = 0){
-                this.imageUrl = "url(src/images/Clear_Night.png)"
+            else if (this.isday == "0"){
+                this.imageUrl = "background-image: url(src/images/Clear_Night.png";
+                this.textcolour = "white";
             }
-            css('background-image', this.imageUrl);
         }
-        else if (weatherCode = 2){
+        else if (this.weatherCode == "2"){
             this.weather_type = "Partly Cloudy"
-            if(isday = 1){
-                this.imageUrl = "url(src/images/Cloudy_Day.png)"
+            if(this.isday == "1"){
+                this.imageUrl = "background-image: url(src/images/Cloudy_Day.png";
+                this.textcolour = "black";
             }
-            else if (isday = 0){
-                this.imageUrl = "url(src/images/Cloudy_Night.png)"
+            else if (this.isday == "0"){
+                this.imageUrl = "background-image: url(src/images/Cloudy_Night.png";
+                this.textcolour = "black";
             }
-            css('background-image', this.imageUrl);
         }
-        else if (weatherCode = 3){
+        else if (this.weatherCode == "3"){
             this.weather_type = "Overcast"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 45){
+        else if (this.weatherCode == '45'){
             this.weather_type = "Foggy"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 48){
+        else if (this.weatherCode == "48"){
             this.weather_type = "Rime Fog"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 51){
+        else if (this.weatherCode == "51"){
             this.weather_type = "Light Drizzle"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 53){
+        else if (this.weatherCode == "53"){
             this.weather_type = "Moderate Drizzle"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 55){
+        else if (this.weatherCode == "55"){
             this.weather_type = "Dense Drizzle"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 56){
+        else if (this.weatherCode == "56"){
             this.weather_type = "Light Freezing Drizzle"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 57){
+        else if (this.weatherCode == "57"){
             this.weather_type = "Dense Freezing Drizzle"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 61){
-            this.weather_type = "LIght Rain"
+        else if (this.weatherCode == "61"){
+            this.weather_type = "Light Rain"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 63){
+        else if (this.weatherCode == "63"){
             this.weather_type = "Moderate Rain"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 65){
+        else if (this.weatherCode == "65"){
             this.weather_type = "Heavy Rain"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 66){
+        else if (this.weatherCode == "66"){
             this.weather_type = "Freezing Light Rain"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 67){
+        else if (this.weatherCode == "67"){
             this.weather_type = "Freezing Heavy Rain"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 71){
+        else if (this.weatherCode == "71"){
             this.weather_type = "Slight Snow"
+            if(this.isday == "1"){
+ 
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 73){
+        else if (this.weatherCode == "73"){
             this.weather_type = "Moderate Snow"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 75){
+        else if (this.weatherCode == "75"){
             this.weather_type = "Heavy Snow"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 77){
+        else if (this.weatherCode == "77"){
             this.weather_type = "Snow Grains"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 80){
+        else if (this.weatherCode == "80"){
             this.weather_type = "Slight Rain Showers"
+            if(this.isday == "1"){
+ 
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 81){
+        else if (this.weatherCode == "81"){
             this.weather_type = "Moderate Rain Showers"
+            if(this.isday == "1"){
+
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 82){
+        else if (this.weatherCode == "82"){
             this.weather_type = "Violent Rain Showers"
+            if(this.isday == "1"){
+            
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 85){
+        else if (this.weatherCode == "85"){
             this.weather_type = "Light Snow Showers"
+            if(this.isday == "1"){
+            
+            }
+            else if (this.isday == "0"){
+
+            }
         }
-        else if (weatherCode = 86){
+        else if (this.weatherCode == "86"){
             this.weather_type = "Heavy Snow Showers"
+            if(this.isday == "1"){
+                
+            }
+            else if (this.isday == "0"){
+
+            }
+            
         }
     }
-
-    
-
 
         render() {
             if(this._data){
-            return html`
-            <div id='container'>
-                <p id='title'>Current Weather</p>
-                <p id='place'>Sydney</p>
-                <p id='Temp'>${this._data.current_weather.temperature}&deg;C</p>
-                <p id='Time'>${(this._data.current_weather.time).slice(0, 10)}</p>
-                <p id='weather'>${this.weather_type}</p>
-            </div>
-            `;
+                                
+                return html`
+                <div id='container' style="${this.imageUrl})">
+                    <p id='title' style="color:${this.textcolour};">Current Weather:</p>
+                    <p id='place'>Sydney</p>
+                    <p id='Temp'>${this._data.current_weather.temperature}&deg;C</p>
+                    <p id='Date'>${(this._data.current_weather.time).slice(0, 10)}</p>
+                    <p id='Time'>${(this._data.current_weather.time).slice(11, 16)}</p>
+                    <p id='weather'>${this.weather_type}</p>
+
+                </div>
+                `;
+
+
+                
             }
         }
     
