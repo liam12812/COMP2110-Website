31ee020e56bb8830d1239f0065b54a892aaf947d
felix.barker <felix.barker03@gmail.com>
2023-05-12 14:48:30 +1000

    Adding slide change functionality

--- src/components/FelixWidget/sunset-widget.js
+++ src/components/FelixWidget/sunset-widget.js
@@ -17,11 +17,9 @@ class SunWidget extends LitElement {
             border: 1px solid white;
             border-radius: 5px;
 
-            width: 100%;
-            height: 100%;
-            padding-left: 20px;
-            padding-right: 20px;
-            padding-bottom: 100%;
+            width: 460px;
+            height: 460px;
+
             margin: auto;
 
             background: url(src/components/FelixWidget/content/vecteezy_vector-illustration-of-mountain-landscapes-in-a-flat-style_8555312.jpg);
@@ -35,24 +33,80 @@ class SunWidget extends LitElement {
 
         .title {
             text-align: center;
-            margin-top: 20px;
-            text-decoration: underline;
 
-            width: 100%;
-            height: 30%;
+            width: 90%;
+            height: 10%;
+
+            margin: auto;
+            margin-top: 20px;
+            margin-bottom: 20px;
         }
 
         .main {
-            width: 80%;
-            height: 80%;
+            width: 100%;
+            height: 70%;
 
-            margin: auto;
+            margin: 0 auto;
+            position: relative;
+        }
+
+        .left-button {
+            position: absolute;
+            top: 33%;
+
+            height: 30%;
+            width: 25px;
+
+            margin-left: 5px;
+
+            background-color: rgba(0, 0, 0, 0);
+            border: none;
+            border-radius: 3px;
+            color: white;
+            font-size: 20px;
+        }
+
+        .left-button:hover {
+            background-color: rgba(0, 0, 0, 0.25);
+        }
+
+        .left-button:active {
+            font-size: 25px;
+        }
+
+        .right-button {
+            position: absolute;
+            left: 93.8%;
+            top: 33%;
+
+            height: 30%;
+            width: 25px;
+
+            background-color: rgba(0, 0, 0, 0);
+            border: none;
+            border-radius: 3px;
+            color: white;
+            font-size: 20px;
+        }
+
+        .right-button:hover {
+            background-color: rgba(0, 0, 0, 0.25);
+        }
+
+        .right-button:active {
+            font-size: 25px;
         }
 
         .sunrise {
+            box-sizing: border-box;
+
             width: 80%;
 
-            margin: auto;
+            position: absolute;
+            left: 10%;
+            top: 8%;
+
+            margin: 0 auto;
             float: left;
 
             text-align: right;
@@ -64,8 +118,15 @@ class SunWidget extends LitElement {
         }
 
         .sunset {
+            box-sizing: border-box;
+
             width: 80%;
-            margin: auto;
+
+            position: absolute;
+            left: 10%;
+            top: 55%;
+
+            margin:  0 auto;
             float: left;
 
             text-align: right;
@@ -92,6 +153,12 @@ class SunWidget extends LitElement {
 
             height: 80px;
             width: 80px;
+        }
+
+        .footer {
+            width: 90%;
+
+            margin: auto;
         }
 
         #creditAPI {
@@ -135,17 +202,47 @@ class SunWidget extends LitElement {
     render() {  
         if(this._data) {
             return html`
-                <h3 class="title">Sunrise/set Times</h3>
+
+            <script>
+            var slideIndex = 1;
+            showDivs(slideIndex);
+            
+            function plusDivs(n) {
+              showDivs(slideIndex += n);
+            }
+            
+            showDivs(n){
+                var i;
+                var x = document.getElementsByClassName("main");
+                if (n > x.length) {slideIndex = 1}
+                if (n < 1) {slideIndex = x.length}
+                for (i = 0; i < x.length; i++) {
+                    x[i].style.display = "none";  
+                }
+                x[slideIndex-1].style.display = "block";
+            }
+            
+            }
+            </script>
+
+
+                <h3 class="title">Sun Position</h3>
+
+                    <div class="main">
+                        <button class="left-button" onclick="">&#10094;</button>
+                        <button class="right-button" onclick="">&#10095;</button>
                         <p class="sunrise">
-                        <img class="icon" id="sunrise-icon" src="src/components/FelixWidget/content/vecteezy_sunrise-sun-line-icon-vector-illustration-logo_.jpg">
-                        Sunrise <br>
-                        ${this._data.results.sunrise}
-                    </p>
-                    <p class="sunset">
-                        <img class="icon" id="sunset-icon" src="src/components/FelixWidget/content/vecteezy_sunset-sun-line-icon-vector-illustration-logo_.jpg">
-                        Sunset <br>    
-                        ${this._data.results.sunset}
-                    </p>
+                            <img class="icon" id="sunrise-icon" src="src/components/FelixWidget/content/vecteezy_sunrise-sun-line-icon-vector-illustration-logo_.jpg">
+                            Sunrise <br>
+                            ${this._data.results.sunrise}
+                            </p>
+                        <p class="sunset">
+                            <img class="icon" id="sunset-icon" src="src/components/FelixWidget/content/vecteezy_sunset-sun-line-icon-vector-illustration-logo_.jpg">
+                            Sunset <br>    
+                            ${this._data.results.sunset}
+                        </p>
+                    </div>
+
                 <section class="footer">
                 <p id="creditAPI"> 
                     Powered by <a href="https://sunrisesunset.io/">SunriseSunset.io</a>
@@ -156,7 +253,26 @@ class SunWidget extends LitElement {
             `;
         } else {
             return html`
-                <p>Loading SunWidget</p>
+            <h3 class="title">Sunrise/set Times</h3>
+            <div class="main">
+                <p class="sunrise">
+                    <img class="icon" id="sunrise-icon" src="src/components/FelixWidget/content/vecteezy_sunrise-sun-line-icon-vector-illustration-logo_.jpg">
+                    Sunrise <br>
+                    Loading...
+                    </p>
+                <p class="sunset">
+                    <img class="icon" id="sunset-icon" src="src/components/FelixWidget/content/vecteezy_sunset-sun-line-icon-vector-illustration-logo_.jpg">
+                    Sunset <br>    
+                    Loading...
+                </p>
+            </div>
+        <section class="footer">
+        <p id="creditAPI"> 
+            Powered by <a href="https://sunrisesunset.io/">SunriseSunset.io</a>
+        <p id="creditIMG">
+            <a href="https://www.vecteezy.com/free-vector/sunrise">Sunrise Vectors by Vecteezy</a>
+        </p>
+        </section>
             `;
         }
     }
