66ca0129b3625f0c51f1d66417d6411aae2a2083
liam12812 <79492075+liam12812@users.noreply.github.com>
2023-05-25 18:43:14 +1000

    d

--- src/components/FelixWidget/sunset-widget.js
+++ src/components/FelixWidget/sunset-widget.js
@@ -227,7 +227,6 @@ class SunWidget extends LitElement {
 
     render() { 
         localStorage.setItem("Timezone", this._data.results.timezone);
-        console.log(localStorage.getItem("Timezone"));
         if(this._data && (this.slide % 2 == 0)) {
             return html`
                 <div id="container" style = "background-image: url(src/components/FelixWidget/content/vecteezy_vector-illustration-of-mountain-landscapes-in-a-flat-style_8555312.jpg)">
