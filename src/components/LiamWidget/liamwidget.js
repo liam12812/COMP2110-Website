254d4ab8973e2f9f8840e072c35bbba7cd50651f
liam12812 <79492075+liam12812@users.noreply.github.com>
2023-05-27 15:50:18 +1000

    d

--- src/components/LiamWidget/liamwidget.js
+++ src/components/LiamWidget/liamwidget.js
@@ -221,8 +221,6 @@ class WeatherWidget extends LitElement {
     }
 
     _fetch () {
-        this.timezone = sessionStorage.getItem("Timezone");
-        console.log(this.timezone);
         const url = `${WeatherWidget.BASE_URL}latitude=${this.Latitude}&longitude=${this.Longitude}&current_weather=true&timezone=${this.timezone}&hourly=temperature_2m,apparent_temperature,precipitation_probability,precipitation&daily=temperature_2m_max,temperature_2m_min&forecast_days=1`
         console.log(url);
         fetch(url)
